23c2f47dce38415919eb4983857956a542f90aee
maoyuchaxue <wangjt15@mails.tsinghua.edu.cn>
2018-03-02 15:47:30 +0800

    lab4 finished, also fixed a bug in lab2

--- labcodes/lab4/kern/mm/vmm.c
+++ labcodes/lab4/kern/mm/vmm.c
@@ -361,10 +361,11 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
     *   mm->pgdir : the PDT of these vma
     *
     */
-#if 0
+#if 1
     /*LAB3 EXERCISE 1: YOUR CODE*/
-    ptep = ???              //(1) try to find a pte, if pte's PT(Page Table) isn't existed, then create a PT.
+    ptep = get_pte(mm->pgdir, addr, 1);              //(1) try to find a pte, if pte's PT(Page Table) isn't existed, then create a PT.
     if (*ptep == 0) {
+        pgdir_alloc_page(mm->pgdir, addr, perm);
                             //(2) if the phy addr isn't exist, then alloc a page & map the phy addr with logical addr
 
     }
@@ -382,10 +383,11 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
     */
         if(swap_init_ok) {
             struct Page *page=NULL;
-                                    //(1）According to the mm AND addr, try to load the content of right disk page
+            swap_in(mm, addr, &page);                        //(1）According to the mm AND addr, try to load the content of right disk page
                                     //    into the memory which page managed.
-                                    //(2) According to the mm, addr AND page, setup the map of phy addr <---> logical addr
-                                    //(3) make the page swappable.
+            page_insert(mm->pgdir, page, addr, perm);                        //(2) According to the mm, addr AND page, setup the map of phy addr <---> logical addr
+            swap_map_swappable(mm, addr, page, 1);                        //(3) make the page swappable.
+            page->pra_vaddr=addr;
         }
         else {
             cprintf("no swap_init_ok but ptep is %x, failed\n",*ptep);
